6f63fae2a219fa89927c3a109fdeeb9fc7e107ab
U-Alan\Chris <christianqw@gmail.com>
2015-10-02 20:27:03 -0300

    send and resive Json from cors

--- spring/cors_rest_service/public/hello.js
+++ spring/cors_rest_service/public/hello.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
   $("#send").on('click', function(e) {
     $.ajax({
-        url: "http://localhost:8080/greeting"
+        type: 'post',
+        dataType: 'json',
+        data: $("#name_imputs").serializeArray(),
+        url: "http://localhost:8080/greeting"        
     }).then(function(data, status, jqxhr) {
        $('.greeting-id').append(data.id);
        $('.greeting-content').append(data.content);
        console.log(jqxhr);
     });
   });
-  
+
 });
