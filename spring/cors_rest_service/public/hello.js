a41beefdfd2a935ac61828a2e28d8b5479a07950
U-Alan\Chris <christianqw@gmail.com>
2015-10-05 17:14:00 -0300

    cors_rest_service dejo de andar, se comienza el proyecto nuevamente dentro de CORS

--- spring/cors_rest_service/public/hello.js
+++ spring/cors_rest_service/public/hello.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
-  $("#send").on('click', function(e) {
     $.ajax({
-        type: 'post',
-        dataType: 'json',
-        data: $("#name_imputs").serializeArray(),
-        url: "http://localhost:8080/greeting"        
+        url: "http://localhost:8080/greeting"
     }).then(function(data, status, jqxhr) {
        $('.greeting-id').append(data.id);
        $('.greeting-content').append(data.content);
        console.log(jqxhr);
     });
-  });
-
 });
+
+
+/*
+$.ajax({
+    type: 'post',
+    dataType: 'json',
+    data: $("#name_imputs").serializeArray(),
+    url: "http://localhost:8080/greeting"
+}).then(function(data, status, jqxhr) {
+   $('.greeting-id').append(data.id);
+   $('.greeting-content').append(data.content);
+   console.log(jqxhr);
+   });*/
